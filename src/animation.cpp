0a3b8f81ada33b08a9b3c76800e65bd7d1c26045
mfaseeh1717 <mfaseeh1717@mail.com>
2026-07-19 11:49:00 +0500

    Finally completed the two-dimensional character animation after testing all the methods other than bound checking that is the responsibility of the user

--- src/animation.cpp
+++ src/animation.cpp
@@ -4,7 +4,6 @@ void Animations::One_D::AnimationManager::draw(Vector2 pos, float scale, float r
 {
     DrawTextureEx(animation_frames[anim_var.curr_index / anim_var.animation_speed], pos, rot, scale, col);
 }
-
 void Animations::One_D::AnimationManager::update()
 {
     anim_var.curr_index++;
@@ -13,7 +12,6 @@ void Animations::One_D::AnimationManager::update()
         anim_var.curr_index = 0;
     }
 }
-
 void Animations::One_D::AnimationManager::clear_vram()
 {
     for (auto i{0uz}; i < anim_var.total_frames; ++i)
@@ -32,5 +30,19 @@ void Animations::Two_D::AnimationManager::clear_vram()
         }
     }
 }
-
 void Animations::Two_D::AnimationManager::setAnimation(const uint8_t& newAnimation) {   anim_comp.curr_animation = newAnimation;    }
+void Animations::Two_D::AnimationManager::draw(const Vector2& pos, const float& rot, const float& scale)
+{
+    DrawTextureEx(entity_animations[anim_comp.curr_animation][anim_comp.curr_index / anim_comp.animation_speed],
+    pos, rot, scale, WHITE);
+}
+void Animations::Two_D::AnimationManager::update()
+{
+    anim_comp.curr_index++;
+    if (anim_comp.curr_index >= (anim_comp.animation_speed * 
+        anim_comp.total_variant_frames[anim_comp.curr_animation]))
+        {
+            anim_comp.curr_index = 0;
+        }
+        std::cout << anim_comp.curr_index << std::endl;
+}
