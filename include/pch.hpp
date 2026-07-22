35ba22ee0bce9e83ad1083b81e8f21ec5266c75d
mfaseeh1717 <mfaseeh1717@mail.com>
2026-07-22 11:47:10 +0500

    Added a pre-compiled header for faster build times

---
+++ include/pch.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+#include <raylib.h>
+#include <string>
+#include <cstdint>      // required for fixed size integers
+#include <type_traits>      // to treat specific types with templates
+#include <variant>      // declaring compile-time variables and compile-time opt.
+#include <cmath>
--- include/raylib-api.hpp
+++ include/raylib-api.hpp
@@ -1,13 +1,6 @@
 #pragma once
 
-#include <iostream>
-#include <raylib.h>
-#include <string>
-#include <cstdint>      // required for fixed size integers
-#include <type_traits>      // to treat specific types with templates
-#include <variant>      // declaring compile-time variables and compile-time opt.
-#include <cmath>
-
+#include "pch.hpp"
 
 // all the code related to animation management
 namespace Animations
